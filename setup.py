3fd695884fc72b3e0cb8b07181735901ad90530f
satorunnlg <satorunnlg@gmail.com>
2025-02-14 01:39:48 +0900

    setup情報修正

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     install_requires=["redis"],
     python_requires=">=3.7",
     author="Your Name",
-    author_email="your.email@example.com",
+    author_email="satorunnlg@gmail.com",
     description="A Redis-based persistent model class for Python",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/redistorage",  # GitHub URL
+    url="https://github.com/satorunnlg/redistorage.git",  # GitHub URL
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
